3bc4ae42b22279e42ee5bb66c1bcfbd39acaf27a
kabir-coderex <kabir@coderex.co>
2026-01-12 11:34:37 +0600

    Images on bottom line

--- apps/web/src/components/sections/GallerySection.tsx
+++ apps/web/src/components/sections/GallerySection.tsx
@@ -13,10 +13,13 @@ const GallerySection = () => {
     ];
 
     const images = [
+        '/gallery-5.png',
         '/gallery-1.png',
         '/gallery-2.png',
         '/gallery-12.png',
+        '/gallery-12.png',
         '/gallery-3.png',
+        '/gallery-5.png',
         '/gallery-5.png',
         '/gallery-4.png',
         '/gallery-6.png',
