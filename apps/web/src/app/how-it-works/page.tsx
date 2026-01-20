7d6e6901ad9107972195f39f0f19fa8523cb21f7
kabir-coderex <kabir@coderex.co>
2026-01-20 11:11:52 +0600

    feat: add "How it works" and "Sign in" pages with basic structure

---
+++ apps/web/src/app/how-it-works/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { memo } from 'react';
+
+
+const page = () => {
+    return (
+        <>
+            How it works not working
+        </>
+    );
+};
+
+export default memo(page);
