3637523c71a667d353c53c49380366bcdb150591
kabir-coderex <kabir@coderex.co>
2026-01-23 09:20:28 +0600

    fix the sanity deploy issue

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'tfihif9au51gljbrj4vx6yfe',
+    projectId: '90a20xmm',
     dataset: 'production'
   },
   deployment: {
