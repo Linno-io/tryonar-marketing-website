24869ef86f3f3fdc15f98ce688ea04431c22875b
abbelal <belal@coderex.co>
2026-06-05 17:57:11 +0600

    footer and cta dynamic

--- apps/web/src/components/sections/CTASection.tsx
+++ apps/web/src/components/sections/CTASection.tsx
@@ -48,7 +48,7 @@ export default function CTASection({ data }: CTAProps) {
     }
 
     return (
-        <section className="relative overflow-hidden bg-[#050308] pb-24 md:pb-32">
+        <section className="relative overflow-hidden bg-[#050308] xl:pb-27 lg:pb-20 md:pb-15 pb-13">
 
             {/* 1. DOTTED HEADER & LINE (Z-index 20) */}
             <div
@@ -69,17 +69,17 @@ export default function CTASection({ data }: CTAProps) {
             />
 
             {/* 3. CONTENT AREA */}
-            <Container className="relative z-30 pt-[100px]">
+            <Container className="tryon-container relative z-30 pt-13 md:pt-25">
                 <div className="flex flex-col items-center text-center mx-auto px-4">
 
-                    <Heading level={2} className="text-4xl md:text-6xl font-bold text-white mb-2.5 tracking-tight leading-tight">
+                    <Heading level={2} className="2xl:text-5xl lg:text-4xl text-3xl font-bold text-white mb-2.5 tracking-tight leading-tight">
                         {title} 
                         {highlightText && (
                             <span className='text-[#998188]'>{' ' + highlightText}</span>
                         )}
                     </Heading>
 
-                    <p className="text-gray-400 text-lg md:text-xl mb-12 font-normal leading-relaxed">
+                    <p className="text-gray-400 text-[15px] md:text-[18px] xl:text-[19px] mb-8 md:mb-12 font-normal leading-relaxed max-w-[867px] mx-auto">
                         {description}
                     </p>
 
--- apps/web/src/components/ui/Container.tsx
+++ apps/web/src/components/ui/Container.tsx
@@ -23,12 +23,12 @@ export default function Container(props: ContainerProps) {
         sm: 'max-w-screen-sm',
         md: 'max-w-screen-md',
         lg: 'max-w-screen-lg',
-        xl: 'max-w-full lg:max-w-[1190px] xl:max-w-[1300px]',
+        xl: 'max-w-full sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1330px]',
         full: 'max-w-full',
         custom: custom,
     }
 
-    const paddingClass = padding ? 'px-2.5 sm:px-3' : ''
+    const paddingClass = padding ? 'px-3.5' : ''
     const borderClass = withBorder ? 'border-l border-r border-[#EEEDF2]' : ''
     const classes = `mx-auto w-full ${sizes[size]} ${paddingClass} ${borderClass} ${className}`
     
